04a6e7317c5b9a7627916870db6d6778463f5b31
Aman <54789531+eppisai@users.noreply.github.com>
2024-02-03 19:03:17 +0530

    fek do

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')  # Replace 'myproject' with your project name
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')  # Replace 'myproject' with your project name
 
 application = get_wsgi_application()
